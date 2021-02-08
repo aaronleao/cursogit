ed2af822ece9be2910576ecae12b1dd150bbe8cb
Carlos Estombelo Montesco <estombelo@dcomp.ufs.br>
2021-02-08 12:33:00 -0300

    Adiconado carlos_estombelo.h e alteracao de main.c

---
+++ carlosestombelo.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void apresentacaocarlosestombelo(){
+    printf("colaborador: Carlos Estombelo\n");
+    
+}
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 #include "aaron.h"
-
+#include "carlosestombelo.h"
 
 int main(){
 	printf("Hello World\n");
 	apresentacaoAaron();
+	apresentacaocarlosestombelo();
+	
 }
